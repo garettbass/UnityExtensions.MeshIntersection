42a09de7d4d58181b4cc488b1e0bbffd44f03849
Garett Bass <garett.bass@king.com>
2018-08-22 16:52:35 -0700

    initial commit

---
+++ Editor/MeshIntersectionSceneGUI.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+using Object = UnityEngine.Object;
+
+namespace UnityExtensions
+{
+
+    public static class MeshIntersectionTestGUI
+    {
+
+        [InitializeOnLoadMethod]
+        private static void InitializeOnLoad()
+        {
+            SceneView.onSceneGUIDelegate += OnSceneGUI;
+        }
+
+        private static void OnSceneGUI(SceneView sceneView)
+        {
+            switch (sceneView.cameraMode.drawMode) {
+                case DrawCameraMode.Wireframe:
+                case DrawCameraMode.TexturedWire:
+                    DrawMeshIntersectionGUI();
+                    break;
+                default: break;
+            }
+        }
+
+        private static Ray MouseRay;
+        private static MeshIntersection MouseOverIntersection;
+
+        public static void DrawMeshIntersectionGUI()
+        {
+            SceneView.RepaintAll();
+            var @event = Event.current;
+            var mousePosition = @event.mousePosition;
+
+            if (@event.type == EventType.MouseMove)
+            {
+                var mouseRay = HandleUtility.GUIPointToWorldRay(mousePosition);
+
+                var meshRenderers =
+                    Object
+                    .FindObjectsOfType<MeshRenderer>()
+                    .Where(IsVisibleMeshRenderer);
+
+                MouseRay = mouseRay;
+                MouseOverIntersection.Raycast(mouseRay, meshRenderers);
+
+                return;
+            }
+
+            if (@event.type == EventType.Repaint)
+            {
+                var normal = MouseRay.direction;
+                var position = MouseRay.origin + MouseRay.direction * 2f;
+                var size = HandleUtility.GetHandleSize(position) / 10f;
+                Handles.color = Color.black;
+
+                if (MouseOverIntersection.found)
+                {
+                    var go = MouseOverIntersection.gameObject;
+                    var meshRenderer = go.GetComponent<MeshRenderer>();
+                    var bounds = meshRenderer.bounds;
+                    var c = bounds.center;
+                    Handles.color = Color.magenta;
+                    Handles.DrawWireCube(c, bounds.size);
+
+                    var v0 = MouseOverIntersection.vertex0;
+                    var v1 = MouseOverIntersection.vertex1;
+                    var v2 = MouseOverIntersection.vertex2;
+
+                    Handles.color = Color.red;
+                    Handles.DrawSolidDisc(v0, normal, size / 2);
+
+                    Handles.color = Color.green;
+                    Handles.DrawSolidDisc(v1, normal, size / 2);
+
+                    Handles.color = Color.Lerp(Color.blue, Color.cyan, 0.5f);
+                    Handles.DrawSolidDisc(v2, normal, size / 2);
+
+                    Handles.color = Color.Lerp(Color.yellow, Color.clear, 0.25f);
+                    Handles.DrawAAConvexPolygon(v0, v1, v2);
+
+                    var p0 = MouseOverIntersection.position;
+                    var n = MouseOverIntersection.normal;
+                    var p1 = p0 + n * size * 10;
+                    Handles.color = Color.magenta;
+                    Handles.DrawSolidDisc(p0, n, size / 2);
+                    Handles.DrawLine(p0, p1);
+
+                    go = PrefabUtility.FindPrefabRoot(go);
+                    Handles.BeginGUI();
+                    GUILabel(p1, go.name);
+                    Handles.EndGUI();
+                }
+            }
+        }
+
+        private static bool IsNavMeshRenderer(MeshRenderer meshRenderer)
+        {
+            var material = meshRenderer.sharedMaterial;
+            var materialName = material?.name ?? string.Empty;
+            return materialName.Contains("navmesh_mat");
+        }
+
+        private static bool IsVisibleMeshRenderer(MeshRenderer meshRenderer)
+        {
+            if (!meshRenderer.enabled)
+                return false;
+            if (!meshRenderer.isVisible)
+                return false;
+            return !IsNavMeshRenderer(meshRenderer);
+        }
+
+        private static GUIContent TempContent = new GUIContent();
+
+        private static void
+        GUILabel(Vector2 center, string label, GUIStyle style = null)
+        {
+            if (style == null)
+                style = EditorStyles.helpBox;
+            var content = TempContent;
+            content.text = label;
+            var size = style.CalcSize(content);
+            var origin = center - size / 2;
+            var rect = new Rect(origin, size);
+            GUI.Label(rect, label, style);
+        }
+
+        private static void
+        GUILabel(Vector3 center, string label, GUIStyle style = null)
+        {
+            GUILabel(GUIPoint(center), label, style);
+        }
+
+        private static Vector2
+        GUIPoint(Vector3 worldPoint)
+        {
+            return HandleUtility.WorldToGUIPoint(worldPoint);
+        }
+
+        private static Vector3
+        GUIPoint3(Vector3 worldPoint)
+        {
+            var p = GUIPoint(worldPoint);
+            return new Vector3(p.x, p.y);
+        }
+
+    }
+
+}
---
+++ MeshIntersection.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityExtensions
+{
+
+    public struct MeshIntersection
+    {
+        public bool found;
+        public GameObject gameObject;
+        public float distance;
+        public Vector3 position;
+        public Vector3 normal;
+        public Vector3 vertex0, vertex1, vertex2;
+        public Vector3 normal0, normal1, normal2;
+
+        public void Reset() { this = new MeshIntersection(); }
+
+        public bool Raycast(Ray worldRay)
+        {
+            var meshRenderers = Object.FindObjectsOfType<MeshRenderer>();
+            return Raycast(worldRay, meshRenderers);
+        }
+
+        public bool Raycast(Ray worldRay, IEnumerable<MeshRenderer> meshRenderers)
+        {
+            Reset();
+            var meshIntersection = default(MeshIntersection);
+            var nearestDistance = Mathf.Infinity;
+            foreach (var meshRenderer in meshRenderers)
+            {
+                if (meshIntersection.Raycast(worldRay, meshRenderer) &&
+                    meshIntersection.distance < nearestDistance)
+                {
+                    nearestDistance = meshIntersection.distance;
+                    this = meshIntersection;
+                }
+            }
+            return found;
+        }
+
+        public bool Raycast(Ray worldRay, MeshRenderer meshRenderer)
+        {
+            if (meshRenderer.bounds.IntersectRay(worldRay)) 
+            {
+                var meshFilter = meshRenderer.GetComponent<MeshFilter>();
+                return Raycast(worldRay, meshFilter);
+            }
+            Reset();
+            return false;
+        }
+
+        public bool Raycast(Ray worldRay, MeshFilter meshFilter)
+        {
+            if (meshFilter != null)
+            {
+                var mesh = meshFilter.sharedMesh;
+                var meshTransform = meshFilter.transform;
+                if (Raycast(worldRay, mesh, meshTransform))
+                {
+                    gameObject = meshFilter.gameObject;
+                    return true;
+                }
+            }
+            Reset();
+            return false;
+        }
+
+        public bool Raycast(Ray worldRay, Mesh mesh, Transform meshTransform)
+        {
+            if (meshTransform != null)
+            {
+                var meshMatrix = meshTransform.localToWorldMatrix;
+                return Raycast(worldRay, mesh, meshMatrix);
+            }
+            Reset();
+            return false;
+        }
+
+        public bool Raycast(Ray worldRay, Mesh mesh, Matrix4x4 meshMatrix)
+        {
+            Reset();
+            distance = Mathf.Infinity;
+            var meshScale = meshMatrix.lossyScale;
+            var normalScale =
+                (meshScale.x * meshScale.y + meshScale.z) > 0
+                ? +1f : -1f;
+            var indices = mesh.triangles;
+            var normals = mesh.normals;
+            var vertices = mesh.vertices;
+            var n = indices.Length;
+            for (int i = 0; i < n;)
+            {
+                var i0 = indices[i++];
+                var i1 = indices[i++];
+                var i2 = indices[i++];
+
+                var v0 = vertices[i0];
+                var v1 = vertices[i1];
+                var v2 = vertices[i2];
+
+                v0 = meshMatrix.MultiplyPoint(v0);
+                v1 = meshMatrix.MultiplyPoint(v1);
+                v2 = meshMatrix.MultiplyPoint(v2);
+
+                var plane = new Plane(v0, v1, v2);
+
+                var hitDistance = 0f;
+                if (!plane.Raycast(worldRay, out hitDistance))
+                    continue;
+
+                if (hitDistance < 0 || hitDistance > distance)
+                    continue;
+
+                var hitPosition = worldRay.GetPoint(hitDistance);
+
+                var r = hitPosition - v0;
+
+                var edge0 = v2 - v0;
+                var edge1 = v1 - v0;
+
+                var dot00 = Vector3.Dot(edge0, edge0);
+                var dot01 = Vector3.Dot(edge0, edge1);
+                var dot11 = Vector3.Dot(edge1, edge1);
+
+                var coeff = 1f / (dot00 * dot11 - dot01 * dot01);
+
+                var dot02 = Vector3.Dot(edge0, r);
+                var dot12 = Vector3.Dot(edge1, r);
+
+                var u = coeff * (dot11 * dot02 - dot01 * dot12);
+                var v = coeff * (dot00 * dot12 - dot01 * dot02);
+
+                if ((u >= 0) && (v >= 0) && ((u + v) < 1))
+                {
+                    found = true;
+                    distance = hitDistance;
+                    position = hitPosition;
+                    normal = plane.normal * normalScale;
+                    vertex0 = v0;
+                    vertex1 = v1;
+                    vertex2 = v2;
+                    normal0 = normals[i0];
+                    normal1 = normals[i1];
+                    normal2 = normals[i2];
+                }
+            }
+            return found;
+        }
+
+    }
+
+}
